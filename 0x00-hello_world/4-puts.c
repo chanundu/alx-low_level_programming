445b79adc0c735ed1ff6a31ac748412fc12e7d83
chanundu <chanundu@gmail.com>
2022-10-17 08:47:30 -0700

    adding files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - This is the main entry point
+ *
+ *Description: using the main function
+ *Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
