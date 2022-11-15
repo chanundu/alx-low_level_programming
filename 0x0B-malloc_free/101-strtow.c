ee80c91a1c931eafa403ef7bf00a29b20c7b7d59
chanundu <chanundu@gmail.com>
2022-11-15 09:00:26 -0800

    adding file 5

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,7 +31,7 @@ int count_word(char *s)
 /**
  * **strtow - splits a string into words
  * @str: string to split
- * 
+ *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
@@ -54,21 +54,21 @@ char **strtow(char *str)
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
 			{
-				if (c)
-				{
-					end = i;
-					tmp = (char *) malloc(sizeof(char) * (c + 1));
-					if (tmp == NULL)
-					return (NULL);
-					while (start < end)
-					*tmp++ = str[start++];
-					*tmp = '\0';
-					spltr[k] = tmp - c;
-					k++;
-					c = 0;
-				}
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+				return (NULL);
+				while (start < end)
+				*tmp++ = str[start++];
+				*tmp = '\0';
+				spltr[k] = tmp - c;
+				k++;
+				c = 0;
 			}
+		}
 		else if (c++ == 0)
 		start = i;
 	}
