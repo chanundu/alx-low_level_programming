112fcc460fbffc4a508deabcf013adac86225674
chanundu <chanundu@gmail.com>
2022-11-15 01:08:30 -0800

    Fixing errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,6 +14,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	else
-		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2]));
+		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
 				return (0);
 }
