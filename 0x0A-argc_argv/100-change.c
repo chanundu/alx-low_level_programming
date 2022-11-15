db0e1a12f29c5233b9180c52b4de5afbff74cdce
chanundu <chanundu@gmail.com>
2022-11-15 01:18:56 -0800

    fixing betty errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,6 +10,7 @@
 int isInteger(const char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -30,27 +31,29 @@ int main(int argc, char const *argv[])
 {
 	int i = 0, coinUsed = 0, coin = 0;
 	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	if (isInteger(argv[1]))
+
+	{
+		i = atoi(argv[1]);
+		while (i > 0 && coin <= 4)
 		{
-			i = atoi(argv[1]);
-			while (i > 0 && coin <= 4)
+			if (i >= coins[coin])
 			{
-				if (i >= coins[coin])
-				{
-					i -= coins[coin];
-					coinUsed++;
-				}
-				else
-				{
-					coin++;
-				}
+				i -= coins[coin];
+				coinUsed++;
+			}
+			else
+			{
+				coin++;
 			}
 		}
-		printf("%i\n", coinUsed);
-		return (0);
+	}
+	printf("%i\n", coinUsed);
+	return (0);
 }
